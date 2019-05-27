4ca6026b71325844547149f9340f0d5595df8679
Ping Dong <ping.dong@live.com>
2019-05-28 10:50:10 +1200

    Init commit

---
+++ src/PingDong.Azure.Telemetry.Testing/TelemetryClient/TelemetryChannelStub.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.ApplicationInsights.Channel;
+
+namespace PingDong.Azure.Telemetry.Testing
+{
+    /// <summary>
+    /// A stub of <see cref="ITelemetryChannel"/>.
+    /// </summary>
+    internal sealed class TelemetryChannelStub : ITelemetryChannel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TelemetryChannelStub"/> class.
+        /// </summary>
+        public TelemetryChannelStub()
+        {
+            OnSend = telemetry => { };
+            OnFlush = () => { };
+            OnDispose = () => { };
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this channel is in developer mode.
+        /// </summary>
+        public bool? DeveloperMode { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating the channel's URI. To this URI the telemetry is expected to be sent.
+        /// </summary>
+        public string EndpointAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to throw an error.
+        /// </summary>
+        public bool ThrowError { get; set; }
+    
+        /// <summary>
+        /// Gets or sets the callback invoked by the <see cref="Send"/> method.
+        /// </summary>
+        public Action<ITelemetry> OnSend { get; set; }
+
+        public Action OnFlush { get; set; }
+
+        public Action OnDispose { get; set; }
+
+        /// <summary>
+        /// Implements the <see cref="ITelemetryChannel.Send"/> method by invoking the <see cref="OnSend"/> callback.
+        /// </summary>
+        public void Send(ITelemetry item)
+        {
+            if (ThrowError)
+            {
+                throw new Exception("test error");
+            }
+
+            OnSend(item);
+        }
+
+        /// <summary>
+        /// Implements the <see cref="IDisposable.Dispose"/> method.
+        /// </summary>
+        public void Dispose()
+        {
+            OnDispose();
+        }
+
+        /// <summary>
+        /// Implements  the <see cref="ITelemetryChannel.Flush" /> method.
+        /// </summary>
+        public void Flush()
+        {
+            OnFlush();
+        }
+    }
+}
---
+++ src/PingDong.Azure.Telemetry.Testing/TelemetryClient/TelemetryHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace PingDong.Azure.Telemetry.Testing
+{
+    public sealed class TelemetryClientHelper
+    {
+        public static TelemetryClient CreateStub()
+        {
+            var configuration = new TelemetryConfiguration();
+            var sendItems = new List<ITelemetry>();
+            configuration.TelemetryChannel = new TelemetryChannelStub { OnSend = item => sendItems.Add(item) };
+            configuration.InstrumentationKey = Guid.NewGuid().ToString();
+            configuration.TelemetryInitializers.Add(new OperationCorrelationTelemetryInitializer());
+            return new TelemetryClient(configuration);
+        }
+    }
+}
